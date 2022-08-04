23826d3f869c074789504376bcd214f2a5dff3ab
shahrokh <shahrokh.r1379@gmail.com>
2022-08-04 22:33:42 +0430

    show items all in CartProductView

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import CardProduct
 from .serializers import CartProductSerializer
 
 
 class CartProductView(APIView):
+    # POST
     def post(self, request):
         serializer = CartProductSerializer(data=request.data)
         if serializer.is_valid():
@@ -14,3 +16,10 @@ class CartProductView(APIView):
         else:
             return Response({"status": "error", "data": serializer.errors},
                 status=status.HTTP_400_BAD_REQUEST)
+
+    # GET
+    def get(self, request):
+        items = CardProduct.objects.all()
+        serializer = CartProductSerializer(items, many=True)
+        return Response({"status":"success", "data": serializer.data},
+                status=status.HTTP_200_OK)
