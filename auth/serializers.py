e507c4d3e2243353590ddc3f53771ed00051018e
shahrokh <shahrokh.r1379@gmail.com>
2022-08-14 21:56:54 +0430

    add field username and password

--- auth/serializers.py
+++ auth/serializers.py
@@ -3,4 +3,14 @@ from rest_framework import serializers
 
 
 class LoginSerializer(serializers.Serializer):
-    pass
+    username = serializers.CharField(
+        label = "Username",
+        write_only =True
+    )
+
+    password = serializers.CharField(
+        label = "Password",
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
