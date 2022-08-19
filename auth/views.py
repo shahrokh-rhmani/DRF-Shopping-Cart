c1cc76377f705df5224b442b73b6f93aa29f23e3
shahrokh <shahrokh.r1379@gmail.com>
2022-08-19 23:37:17 +0430

    create class LoginView

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serialiazers import LoginSerializer
 
-# Create your views here.
+
+class LoginView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = LoginSerializer(data=request.data,
+                context={'request': self.request})
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response(None, status=status.HTTP_202_ACCEPTED)
