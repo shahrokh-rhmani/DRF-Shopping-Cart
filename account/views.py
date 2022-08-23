ad6e91a514c0db4e9fe90275b257c44ac9f002ba
shahrokh <shahrokh.r1379@gmail.com>
2022-08-23 20:23:39 +0430

    fix error login

--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login
+from rest_framework import status
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import LoginSerializer
+
 
 
 class LoginView(APIView):
