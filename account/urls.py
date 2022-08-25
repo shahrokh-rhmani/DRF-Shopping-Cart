d94e9e901f760a7314d377e523899b9a8e34adf9
shahrokh <shahrokh.r1379@gmail.com>
2022-08-25 22:51:32 +0430

    creat path view ProfileView

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, ProfileView
 
 urlpatterns = [
     path('',  LoginView.as_view()),
+    path('profile/', ProfileView.as_view())
 
 ]
