e8bd429a52919c731f875871bcbbc2bf27932f02
shahrokh <shahrokh.r1379@gmail.com>
2022-08-25 22:41:31 +0430

    create serializers UserProfileSerializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
 
 
 class LoginSerializer(serializers.Serializer):
@@ -34,3 +36,11 @@ class LoginSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        
