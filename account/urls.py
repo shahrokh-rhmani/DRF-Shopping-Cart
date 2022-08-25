2a27f51c48fd2f68623ec34ca68ceee97e9c3211
shahrokh <shahrokh.r1379@gmail.com>
2022-08-25 23:00:26 +0430

    fix name url

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LoginView, ProfileView
 
 urlpatterns = [
-    path('',  LoginView.as_view()),
+    path('login/',  LoginView.as_view()),
     path('profile/', ProfileView.as_view())
 
 ]
